f1dc404e6fd299f4345bcbf6d92a197e4c52bc8a
anuverma87 <imanu.verma@gmail.com>
2024-12-19 11:32:24 +0530

    rest files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+      <h1>Troffico</h1>
+      <button>reactButton</button>
+      <Button>BootstarpButton</Button>
+    </div>
+  );
+}
+
+export default App;
